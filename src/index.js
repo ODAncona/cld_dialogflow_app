1418bb8bf26eaabb401c0aec5f422cc289d1e139
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2022-05-31 11:48:19 +0200

    nodejs

--- src/index.js
+++ src/index.js
@@ -1,18 +1,42 @@
 import * as fs from "fs";
-import { parse } from "csv-parse";
-import { get } from "http";
+import {
+  parse
+} from "csv-parse";
+import express from 'express';
 
-const CSV_PATH = "./data/boissons.csv";
+// Configuration
+const CSV_PATH = "../data/boissons.csv";
+let data = [];
+const parser = parse({
+  delimiter: ':'
+}, (err, data) => {
+  console.log(data);
+});
 
-async function getData(filepath) {
-    let ret = [];
-    fs.createReadStream(filepath)
-        .pipe(parse({ columns: true }))
-        .on("data", (row) => {
-            ret.push(row);
-        });
-    return ret;
+// Parsing function
+const getData = async (filepath) => {
+  fs.createReadStream(CSV_PATH).pipe(parser);
 }
 
+const app = express();
+
+// Express BASIC
+app.get('/', (req, res) => {
+  res.send('Hello World');
+})
+
+// Express get price
+app.get('/price', (req, res) => {
+  res.send('1CHF');
+})
+
+// Express get infos
+app.get('/infos', (req, res) => {
+  res.send(true);
+})
+
+// Execution
+console.log("API started...");
+app.listen(3000);
 const mdr = await getData(CSV_PATH);
 console.log(mdr)
